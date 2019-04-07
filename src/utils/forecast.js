e0b44254c539fae864a67f7134e5638515110c04
Shaurya Sisodia <sisodiashaurya@gmail.com>
2019-04-08 00:21:54 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location!', undefined)
         }
         else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.\nThe temperatures are expected to get as high as ' + body.daily.data[0].temperatureHigh + ' with a minimum of '+ body.daily.data[0].temperatureLow + '.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. ' + 'The high today is ' + body.daily.data[0].temperatureHigh + ' degrees with a low of ' + body.daily.data[0].temperatureLow + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })    
 }
