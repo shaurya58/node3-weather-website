5b12e01323b4e5f53683b2f077e79edb2ae59828
Shaurya Sisodia <sisodiashaurya@gmail.com>
2019-04-07 23:49:31 +0530

    Revert "Additional forecast data"
    This reverts commit a8bcde5cacd21047639ca2fb27a89994197ae2c7.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location!', undefined)
         }
         else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.\nThe temperatures are expected to get as high as ' + body.daily.data[0].temperatureHigh + ' with a minimum of '+ body.daily.data[0].temperatureLow + '.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })    
 }
